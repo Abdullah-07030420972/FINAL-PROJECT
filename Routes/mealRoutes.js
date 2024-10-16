e394185e5e93d42096de3f216e09c6c2694e28e2
Adamu Abdullaziz Shuaibu <adamuabdullaziz85@gmail.com>
2024-10-16 09:39:41 -0700

    updated

--- Routes/mealRoutes.js
+++ Routes/mealRoutes.js
@@ -3,15 +3,16 @@
 const express = require("express")
 
 const { mealPlan, get_meal, update_meal, deletemeal } = require("../Controllers/mealController")
+const validateTk = require("../Middleware/validateAuth")
 
 const router = express.Router()
 
 router.post("./meal", mealPlan)
 
-router.get("./getMealPlan/:userId", get_meal)
+router.get("./getMealPlan/:userId", validateTk, get_meal)
 
-router.put("./updateMeal/:userId", update_meal)
+router.put("./updateMeal/:userId", validateTk, update_meal)
 
-router.delete("./deleteplan/:userId", deletemeal)
+router.delete("./deleteplan/:userId", validateTk, deletemeal)
 
 module.exports = router
